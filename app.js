96d913fde44fbdc1c3b123cdebbe11e9192674b6
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-05 16:30:55 -0400

    syned API key

---
+++ app.js
@@ -0,0 +1,14 @@
+// How to request data from an API using JavaScript to jQuery
+
+// There are many ways to do this!
+/// 1. jQuery$.ajax() function
+/// 2. The Browser's fetch function
+/// 3. Using the Axios http client library (google it!)
+
+// creating a shortcut variable
+const URL = "https://www.omdbapi.com/?apikey=a71b148a&t=Die+Hard";
+
+$.ajax(URL).then(function (data) {
+  console.log("Movie data is ready");
+  console.log(data);
+});
